55d88c3f5213c9dad3b86f972c5a0b26bb6fc460
Martin Vilcans <martin@librador.com>
2020-09-29 10:39:53 +0200

    Get contact and proximity events

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,33 @@
-use rapier3d::geometry::{BroadPhase, ColliderSet, NarrowPhase};
-use rapier3d::pipeline::PhysicsPipeline;
-use rapier3d::{dynamics::BodyStatus, dynamics::RigidBodyBuilder, na::Vector3};
 use rapier3d::{
-    dynamics::{IntegrationParameters, JointSet, RigidBodySet},
-    geometry::ColliderBuilder,
+    dynamics::{BodyStatus, IntegrationParameters, JointSet, RigidBodyBuilder, RigidBodySet},
+    geometry::{BroadPhase, ColliderBuilder, ColliderSet, NarrowPhase},
+    na::Vector3,
+    pipeline::{ChannelEventCollector, PhysicsPipeline},
 };
 
 fn main() {
     let mut pipeline = PhysicsPipeline::new();
     let gravity = Vector3::new(0.0, -9.81, 0.0);
 
+    // See https://rapier.rs/docs/user_guides/rust/integration_parameters
     let mut integration_parameters = IntegrationParameters::default();
+    integration_parameters.allowed_linear_error = 0.01; // Allow this much penetration
     integration_parameters.set_inv_dt(100.0);
 
     let mut broad_phase = BroadPhase::new();
     let mut narrow_phase = NarrowPhase::new();
     let mut joints = JointSet::new();
-    // We ignore contact events for now.
-    let event_handler = ();
+    let mut rigid_body_set = RigidBodySet::new();
+    let mut collider_set = ColliderSet::new();
 
-    let rigid_body = RigidBodyBuilder::new(BodyStatus::Dynamic)
+    // Initialize the event collector.
+    let (contact_send, contact_recv) = crossbeam::channel::unbounded();
+    let (proximity_send, proximity_recv) = crossbeam::channel::unbounded();
+    let event_handler = ChannelEventCollector::new(proximity_send, contact_send);
+
+    // Ball
+
+    let ball_body = RigidBodyBuilder::new(BodyStatus::Dynamic)
         // The rigid body translation.
         // Default: zero vector.
         .translation(0.0, 5.0, 0.0)
@@ -39,21 +47,27 @@ fn main() {
         .can_sleep(true)
         .build();
 
-    let mut rigid_body_set = RigidBodySet::new();
-    let body_handle = rigid_body_set.insert(rigid_body);
+    let ball_body_handle = rigid_body_set.insert(ball_body);
 
-    let collider = ColliderBuilder::ball(1.5)
+    let ball_collider = ColliderBuilder::ball(1.5)
         .translation(0.0, 0.0, 0.0) // relative to rigid body
         .friction(0.8)
         .sensor(false)
         .build();
 
-    let mut collider_set = ColliderSet::new();
-    let collider_handle = collider_set.insert(collider, body_handle, &mut rigid_body_set);
+    let ball_collider_handle =
+        collider_set.insert(ball_collider, ball_body_handle, &mut rigid_body_set);
+
+    // Floor
+
+    let floor_body = RigidBodyBuilder::new(BodyStatus::Static).build();
+    let floor_body_handle = rigid_body_set.insert(floor_body);
+    let floor_collider = ColliderBuilder::cuboid(20.0, 0.1, 20.0).build();
+    collider_set.insert(floor_collider, floor_body_handle, &mut rigid_body_set);
 
     for _ in 0..100 {
-        let collider = collider_set.get(collider_handle).unwrap();
-        let body = rigid_body_set.get(body_handle).unwrap();
+        let collider = collider_set.get(ball_collider_handle).unwrap();
+        let body = rigid_body_set.get(ball_body_handle).unwrap();
         println!(
             "collider: {:?} body {:?}",
             collider.position().translation,
@@ -69,5 +83,13 @@ fn main() {
             &mut joints,
             &event_handler,
         );
+
+        while let Ok(proximity_event) = proximity_recv.try_recv() {
+            println!("Got proximity event {:?}", proximity_event);
+        }
+
+        while let Ok(contact_event) = contact_recv.try_recv() {
+            println!("Got contact event {:?}", contact_event);
+        }
     }
 }
